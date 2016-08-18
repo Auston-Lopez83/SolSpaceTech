7962a98840df1804a376f3b2f2b4c94886c0b2e6
Auston Lopez <Auston Lopez>
2016-08-18 15:34:55 -0500

    Getting there

--- playerProfiles/playerProfiles/Program.cs
+++ playerProfiles/playerProfiles/Program.cs
@@ -26,6 +26,7 @@ namespace playerProfiles
 			String[] playersSeasons = new string[50];
 			String[] playersSeasonTeam = new string[50];
 			int playerx = 0;
+			int x = 0;
 
 
 			//end of stuff
@@ -36,32 +37,44 @@ namespace playerProfiles
 			Console.WriteLine("What is the name of the team?");
 			String teamName = Console.ReadLine();
 			Console.WriteLine("Hello Coach {0} Let us start to enter the players names and Info",coach);
-			String yOrn = "y";
-			if (yOrn == "Y"|| yOrn == "y")
+			String yOrn = "Y";
+			while (yOrn == "Y" || yOrn == "y")
 			{
+
+
 				Console.WriteLine("What is the players full name?");
 				playersName[playerx] = Console.ReadLine();
 				Console.WriteLine("What is the players age?");
 				playersAge[playerx] = Console.ReadLine();
 				Console.WriteLine("What is the players Weight? in lbs. please");
 				playersWeight[playerx] = Console.ReadLine();
-				Console.WriteLine("What is the players Hight?Feet first.");
+				Console.WriteLine("What is the players Hight? Feet first.");
 				playersHightFeet[playerx] = Console.ReadLine();
 				Console.WriteLine("Now Inches");
 				playersHightInches[playerx] = Console.ReadLine();
 				Console.WriteLine("Number of seasons they played Rugby");
 				playersSeasons[playerx] = Console.ReadLine();
-				Console.WriteLine("NUmber of Seasons with {0}",teamName);
+				Console.WriteLine("NUmber of Seasons with {0}", teamName);
 				playersSeasonTeam[playerx] = Console.ReadLine();
 				playerx = playerx + 1;
+				Console.WriteLine("Do you still want to add more players (y) or (n)?");
+				yOrn = Console.ReadLine();
+			}
+			{
+				Console.WriteLine("Here is the list of players in your team with their stats.\n \n");
+				Console.WriteLine("\tName\t\t\tAge\tHight\tWeight\tYears Played\tYears with{0}",teamName);
+				while(x<playerx)
+				{
+					Console.WriteLine($"{x,3}" + $"{playersName[x],30}" + $"{playersAge[x],10}" + $"{playersHightFeet[x],1}" + $"{playersHightInches[x],2}" + $"{playersSeasons[x],2}");
+					x = x + 1;
+					                  
+				}
 
+				Console.WriteLine("End of List");
+				
 				
 			}
-			else if (yOrn == "N" || yOrn == "n")
-			{
-				Console.WriteLine("Where is the list of players in your team with their stats.");
-				Console.WriteLine("End of List");
-			}
+
 
 
 		}
