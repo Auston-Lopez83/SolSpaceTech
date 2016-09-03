0ba83f3a6b567d15b459e9b1ba4edc8697fff535
Auston Lopez <Auston Lopez>
2016-09-03 17:34:52 -0500

    Making the UI

--- warhammerRPGCB/warhammerRPGCB/ViewController.designer.cs
+++ warhammerRPGCB/warhammerRPGCB/ViewController.designer.cs
@@ -1,30 +1,34 @@
-﻿//
-// ViewController.designer.cs
-//
-// Created on 8/27/2016
-//
-// Author:
-//       Auston Lopez <MRPOWERZ83@me.com>
-//
-// Copyright (c) 2016 
-//
-//
-// WARNING
+﻿// WARNING
 //
 // This file has been generated automatically by Xamarin Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace warhammerRPGCB
 {
-	[Register("ViewController")]
+	[Register ("ViewController")]
 	partial class ViewController
 	{
-		void ReleaseDesignerOutlets()
+		[Outlet]
+		AppKit.NSPopUpButton careerMenu { get; set; }
+
+		[Outlet]
+		AppKit.NSPopUpButtonCell raceMenu { get; set; }
+		
+		void ReleaseDesignerOutlets ()
 		{
+			if (raceMenu != null) {
+				raceMenu.Dispose ();
+				raceMenu = null;
+			}
+
+			if (careerMenu != null) {
+				careerMenu.Dispose ();
+				careerMenu = null;
+			}
 		}
 	}
 }
-
