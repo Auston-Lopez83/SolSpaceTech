0d5856610e487e8f6608cd6263f502bc03f601e9
Auston Lopez <Auston Lopez>
2016-12-13 15:46:04 -0600

    Lost theRoses

---
+++ TheRoses/Assets/BoardPos.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.EventSystems;
+
+public class BoardPos : MonoBehaviour
+{
+	public Transform flower;
+	public void colorFlower()
+	{
+		flower = transform.GetChild.name;
+	}
+	
+}
+
+
---
+++ TheRoses/Assets/spawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	public Transform[] spawnLocations;
+	public GameObject[] flowerPreFab;
+	public GameObject[] flowerClone;
+
+	void Start()
+	{
+		flowerSpawner ();
+	}
+
+	void flowerSpawner()
+	{
+		//flowerClone[0] = Instantiate (flowerPreFab [0], spawnLocations [0].transform.parent , Quaternion.Euler (0, 0, 0)) as GameObject;
+	}
+}
